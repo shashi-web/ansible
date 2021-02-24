8616b6c377e48527a25fa56e5fe146c7f87f76cb
shashidhar <raoshashidhar.3@gmail.com>
2021-02-24 01:10:02 -0500

    ansible host

---
+++ instance-launch/create-instance.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+component=$1
+
+if [ -z "${component}" ]; then
+  echo "Need an input of component name"
+  exit 1
+fi
+
+STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
+
+if [ "$STATE" != "running" ]; then
+  aws ec2 run-instances  --launch-template LaunchTemplateId=lt-061c8355daa9a4394 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
+  sleep 15
+fi
+
+IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+
+export component
+export IPADDRESS
+
+envsubst <record.json >/tmp/${component}.json
+
+aws route53 change-resource-record-sets --hosted-zone-id Z03431283K8URE8K6UP1G --change-batch file:///tmp/${component}.json
+
+sed -i -e "/${component}/ d" ../inventory
+PUBLIC_IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+echo "${PUBLIC_IPADDRESS} APP=${component}" >>../inventory
+
