b9f5f1eb0e42ccff75dd0c4c08ee0ffb4a0d8131
shashidhar <raoshashidhar.3@gmail.com>
2021-03-22 16:05:27 -0400

    ansible host

--- instance-launch/create-instance.sh
+++ instance-launch/create-instance.sh
@@ -9,14 +9,14 @@ if [ "${component}" == "all" ]; then
       echo "Creating ${component} Server"
       STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
       if [ "$STATE" != "running" ]; then
-      aws ec2 run-instances  --launch-template LaunchTemplateId=lt-0c43aaeb0e08199e0 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" &>/tmp/{component}.log
+      aws ec2 run-instances  --launch-template LaunchTemplateId=lt-061c8355daa9a4394 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]" &>/tmp/{component}.log
       sleep 5
       fi
       IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
       export component
       export IPADDRESS
       envsubst <record.json >/tmp/${component}.json
-      aws route53 change-resource-record-sets --hosted-zone-id Z076258917LZOFQ2TZ491 --change-batch file:///tmp/${component}.json
+      aws route53 change-resource-record-sets --hosted-zone-id Z03431283K8URE8K6UP1G --change-batch file:///tmp/${component}.json
       sed -i -e "/${component}/ d" ../inventory
       PUBLIC_IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
       echo "${PUBLIC_IPADDRESS} APP=${component}" >>../inventory
